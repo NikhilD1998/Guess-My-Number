aff91c183c3ef92f9ad59dfdb227a5957468a736
NikhilD1998 <nikhild.dev.1998@gmail.com>
2023-03-19 10:18:00 +0530

    Added Score Counter

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  } else if (guess == secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+    score++;
+    document.querySelector(".score").textContent = score;
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High";
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too Low ";
+    score--;
+    document.querySelector(".score").textContent = score;
+  }
+});
